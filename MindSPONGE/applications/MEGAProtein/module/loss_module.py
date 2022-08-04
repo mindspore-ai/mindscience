b15d03fa19fb226b1f9e3cea94f478d9ee805428
wzd <wang1@huawei.com>
2022-08-04 20:35:39 +0800

    add loss moduld of MEGA-Assessment

--- MindSPONGE/applications/MEGAProtein/module/loss_module.py
+++ MindSPONGE/applications/MEGAProtein/module/loss_module.py
@@ -15,6 +15,7 @@
 """loss module"""
 
 import mindspore as ms
+import mindspore.communication.management as D
 import mindspore.nn as nn
 import mindspore.numpy as mnp
 from mindspore import Tensor
@@ -25,10 +26,12 @@ from mindsponge.common.geometry import invert_point, quaternion_from_tensor, vec
 from mindsponge.metrics.structure_violations import softmax_cross_entropy, sigmoid_cross_entropy, \
     get_structural_violations, compute_renamed_ground_truth, backbone, sidechain, supervised_chi, \
     local_distance_difference_test
+from mindsponge.metrics.metric import BalancedMSE, BinaryFocal, MultiClassFocal
 
 
 class LossNet(nn.Cell):
     """loss net"""
+
     def __init__(self, config):
         super(LossNet, self).__init__()
         self.config = config
@@ -41,23 +44,23 @@ class LossNet(nn.Cell):
         self.exp_max_resolution = config.heads.experimentally_resolved.max_resolution
         self.exp_res_filter_by_resolution = config.heads.experimentally_resolved.filter_by_resolution
         self.experimentally_weight = config.heads.experimentally_resolved.weight
-        self.exp_res_mask = Tensor(1, ms.float32)\
-            if not self.exp_res_filter_by_resolution or\
+        self.exp_res_mask = Tensor(1, ms.float32) \
+            if not self.exp_res_filter_by_resolution or \
                (self.exp_min_resolution <= self.resolution <= self.exp_max_resolution) else Tensor(0, ms.float32)
 
         self.ael_min_resolution = config.heads.predicted_aligned_error.min_resolution
         self.ael_max_resolution = config.heads.predicted_aligned_error.max_resolution
         self.ael_res_filter_by_resolution = config.heads.predicted_aligned_error.filter_by_resolution
-        self.ael_res_mask = Tensor(1, ms.float32)\
-            if not self.ael_res_filter_by_resolution or\
+        self.ael_res_mask = Tensor(1, ms.float32) \
+            if not self.ael_res_filter_by_resolution or \
                (self.ael_min_resolution <= self.resolution <= self.ael_max_resolution) else Tensor(0, ms.float32)
         self.aligned_one_hot = nn.OneHot(depth=config.heads.predicted_aligned_error.num_bins)
 
         self.plddt_min_resolution = config.heads.predicted_lddt.min_resolution
         self.plddt_max_resolution = config.heads.predicted_lddt.max_resolution
         self.plddt_res_filter_by_resolution = config.heads.predicted_lddt.filter_by_resolution
-        self.plddt_res_mask = Tensor(1, ms.float32)\
-            if not self.plddt_res_filter_by_resolution or\
+        self.plddt_res_mask = Tensor(1, ms.float32) \
+            if not self.plddt_res_filter_by_resolution or \
                (self.plddt_min_resolution <= self.resolution <= self.plddt_max_resolution) else Tensor(0, ms.float32)
         self.plddt_weight = config.heads.predicted_lddt.weight
 
@@ -294,3 +297,207 @@ class LossNet(nn.Cell):
 
         loss = loss * P.Sqrt()(P.ReduceSum()(all_atom_mask[:, 0]))
         return loss, l_fape_side, l_fape_backbone, l_anglenorm, distogram_loss, masked_loss, predict_lddt_loss
+
+
+class LossNetAssessment(nn.Cell):
+    """loss net"""
+
+    def __init__(self, config):
+        super(LossNetAssessment, self).__init__()
+        self.orign_loss = LossNet(config)
+
+        self.cutoff = 15.0
+        self.within_cutoff_clip = 0.3
+        self.beyond_cutoff_clip = 3.0
+        self.beyond_cutoff_weight = 0.2
+        self.regressor_idx = 1
+        self.regressor_weight = 2.
+
+        self.reg_loss_distogram = RegressionLosses(first_break=2., last_break=22.,
+                                                   num_bins=config.model.heads.distogram.num_bins, bin_shift=True,
+                                                   charbonnier_eps=0.1)
+        self.reg_loss_lddt = RegressionLosses(first_break=0., last_break=1.,
+                                              num_bins=config.model.heads.predicted_lddt.num_bins, bin_shift=False,
+                                              charbonnier_eps=1e-5)
+
+        self.binary_focal_loss = BinaryFocal(alpha=0.25, gamma=1., feed_in=False, not_focal=False)
+        self.softmax_focal_loss_lddt = MultiClassFocal(num_class=config.model.heads.predicted_lddt.num_bins,
+                                                       gamma=1., e=0.1, neighbors=2, not_focal=False)
+        self.softmax_focal_loss_distogram = MultiClassFocal(num_class=config.model.heads.distogram.num_bins,
+                                                            gamma=1., e=0.1, neighbors=2, not_focal=False)
+        self.cameo_focal_loss = BinaryFocal(alpha=0.2, gamma=0.5, feed_in=True, not_focal=False)
+
+        self.allreduce = P.AllReduce()
+        self.device_num = D.get_group_size()
+
+    def distogram_loss(self, logits, bin_edges, pseudo_beta, pseudo_beta_mask):
+        """Log loss of a distogram."""
+        positions = pseudo_beta
+        mask = pseudo_beta_mask
+
+        sq_breaks = mnp.square(bin_edges)
+        dist_t = mnp.square(mnp.expand_dims(positions, axis=-2) - mnp.expand_dims(positions, axis=-3))
+        dist2 = P.ReduceSum(True)(dist_t.astype(ms.float32), -1)
+        aa = (dist2 > sq_breaks).astype(ms.float32)
+
+        square_mask = mnp.expand_dims(mask, axis=-2) * mnp.expand_dims(mask, axis=-1)
+        probs = self.softmax(logits)
+        dmat_pred = mnp.sum(probs * mnp.reshape(self.centers, (1, 1, -1)), -1)
+        dist2 = dist2[..., 0]
+        dmat_true = mnp.sqrt(1e-6 + dist2)
+
+        within_cutoff_mask = F.cast(dmat_true < self.cutoff, mnp.float32)
+        within_cutoff_mask *= (1. - mnp.eye(within_cutoff_mask.shape[1]))
+        beyond_cutoff_mask = F.cast(dmat_true > self.cutoff, mnp.float32)
+        beyond_cutoff_mask *= self.beyond_cutoff_weight
+
+        true_bins = P.ReduceSum()(aa, -1)
+        true_bins = true_bins.astype(ms.int32)
+
+        nres, nres, nbins = logits.shape
+        logits = mnp.reshape(logits, (-1, nbins))
+        labels = self.distogram_one_hot(true_bins)
+        labels = mnp.reshape(labels, (-1, nbins))
+
+        error = self.softmax_focal_loss_distogram(logits, labels)
+        error = mnp.reshape(error, (nres, nres))
+        focal_error = within_cutoff_mask * error + beyond_cutoff_mask * error
+
+        focal_loss = (P.ReduceSum()(focal_error * square_mask, (-2, -1)) /
+                      (1e-6 + P.ReduceSum()(square_mask.astype(ms.float32), (-2, -1))))
+
+        error_tuple = self.reg_loss_distogram(dmat_pred, dmat_true)
+        regression_error = error_tuple[1]
+
+        regression_error_clip_within = mnp.clip(regression_error, self.within_cutoff_clip,
+                                                20.) - self.within_cutoff_clip
+        regression_error_clip_beyond = mnp.clip(regression_error, self.beyond_cutoff_clip,
+                                                20.) - self.beyond_cutoff_clip
+
+        regression_error = regression_error_clip_within * within_cutoff_mask + regression_error_clip_beyond \
+                            * beyond_cutoff_mask
+
+        square_mask_off_diagonal = square_mask * (1 - mnp.eye(square_mask.shape[1]))
+
+        regression_loss = (P.ReduceSum()(regression_error * square_mask_off_diagonal, (-2, -1)) /
+                           (1e-6 + P.ReduceSum()(square_mask_off_diagonal.astype(ms.float32), (-2, -1))))
+
+        loss = focal_loss + self.regressor_weight * regression_loss
+
+        dist_loss = loss, focal_loss, regression_loss, dmat_true
+
+        return dist_loss
+
+    def construct(self, distogram_logits, bin_edges, pseudo_beta, pseudo_beta_mask, experimentally_logits,
+                  atom37_atom_exists, all_atom_mask, true_msa, masked_logits, bert_mask,
+                  final_atom14_positions, residue_index, aatype, residx_atom14_to_atom37, lower_bound, upper_bound,
+                  seq_mask, atomtype_radius, final_affines, pae_breaks, pae_logits, angles_sin_cos,
+                  um_angles_sin_cos, backbone_affine_tensor, backbone_affine_mask, atom14_gt_positions,
+                  atom14_alt_gt_positions, atom14_atom_is_ambiguous, atom14_gt_exists, atom14_atom_exists,
+                  atom14_alt_gt_exists, final_atom_positions, all_atom_positions, predicted_lddt_logits, traj,
+                  rigidgroups_gt_frames, rigidgroups_gt_exists, rigidgroups_alt_gt_frames,
+                  pred_frames, pred_positions, sin_cos_true_chi, torsion_angle_mask,
+                  decoy_pseudo_beta, decoy_pseudo_beta_mask, decoy_predicted_lddt_logits, plddt_dist, pred_mask2d):
+        """construct"""
+        _, l_fape_side, l_fape_backbone, l_anglenorm, _, _, predict_lddt_loss = self.orign_loss(
+            distogram_logits, bin_edges, pseudo_beta, pseudo_beta_mask, experimentally_logits,
+            atom37_atom_exists, all_atom_mask, true_msa, masked_logits, bert_mask,
+            final_atom14_positions, residue_index, aatype, residx_atom14_to_atom37, lower_bound, upper_bound,
+            seq_mask, atomtype_radius, final_affines, pae_breaks, pae_logits, angles_sin_cos,
+            um_angles_sin_cos, backbone_affine_tensor, backbone_affine_mask, atom14_gt_positions,
+            atom14_alt_gt_positions, atom14_atom_is_ambiguous, atom14_gt_exists, atom14_atom_exists,
+            atom14_alt_gt_exists, final_atom_positions, all_atom_positions, predicted_lddt_logits, traj,
+            rigidgroups_gt_frames, rigidgroups_gt_exists, rigidgroups_alt_gt_frames,
+            pred_frames, pred_positions, sin_cos_true_chi, torsion_angle_mask, 1.0, 1.0)
+
+        fold_loss = l_fape_side + l_fape_backbone + l_anglenorm + predict_lddt_loss
+
+        lddt_cb = local_distance_difference_test(
+            predicted_points=decoy_pseudo_beta[None, ...],
+            true_points=pseudo_beta[None, ...],
+            true_points_mask=decoy_pseudo_beta_mask[None, ..., None].astype(mnp.float32),
+            cutoff=15.,
+            per_residue=True)[0]
+        lddt_cb = F.stop_gradient(lddt_cb)
+
+        distogram_loss, distogram_focal_loss, distogram_regression_loss, dmat_true = self.distogram_loss(
+            distogram_logits, bin_edges, pseudo_beta, pseudo_beta_mask)
+
+        mask1d = decoy_pseudo_beta_mask
+        mask2d = mnp.expand_dims(mask1d, 1) * mnp.expand_dims(mask1d, 0)
+        error_tuple = self.reg_loss_lddt(plddt_dist, lddt_cb)
+        plddt2_error = error_tuple[self.regressor_idx]
+
+        plddt2_regression_loss = mnp.sum(plddt2_error * mask1d) / (mnp.sum(mask1d) + 1e-8)
+        plddt2_loss = self.regressor_weight * plddt2_regression_loss
+
+        true_mask2d = P.Cast()(dmat_true < self.cutoff, ms.float32)
+        mask_error = self.binary_focal_loss(mnp.reshape(pred_mask2d, (-1,)), mnp.reshape(true_mask2d, (-1,)))
+        mask_error = mnp.reshape(mask_error, true_mask2d.shape)
+        mask_loss = mnp.sum(mask_error * mask2d) / (mnp.sum(mask2d) + 1e-6)
+        confidence_pred = mnp.sum(plddt_dist * mask1d) / (mnp.sum(mask1d) + 1e-6)
+        confidence_gt = mnp.sum(lddt_cb * mask1d) / (mnp.sum(mask1d) + 1e-6)
+        confidence_loss = nn.MSELoss()(confidence_pred, confidence_gt)
+        confidence_loss = mnp.sqrt(confidence_loss + 1e-5)
+
+        cameo_label = F.cast(lddt_cb < 0.6, mnp.float32)
+        cameo_scale = decoy_predicted_lddt_logits[:, 0]
+        cameo_shift = decoy_predicted_lddt_logits[:, 1]
+        cameo_scale = 5. * P.Tanh()(cameo_scale / 5.)
+        decoy_cameo_logit = -F.exp(cameo_scale) * (plddt_dist + cameo_shift - 0.6)
+        cameo_error = self.cameo_focal_loss(decoy_cameo_logit, cameo_label)
+        cameo_loss = mnp.sum(cameo_error * mask1d) / (mnp.sum(mask1d) + 1e-6)
+
+        score_loss = distogram_loss + plddt2_loss + mask_loss + 2.0 * confidence_loss + 2.0 * cameo_loss
+
+        loss = 0.5 * fold_loss + score_loss
+
+        seq_len = F.cast(P.ReduceSum()(pseudo_beta_mask), mnp.float32)
+        loss_weight = mnp.power(seq_len, 0.5)
+        loss_weight_sum = self.allreduce(loss_weight) / self.device_num
+        loss_weight = loss_weight / loss_weight_sum
+        loss_weight *= 64.
+
+        loss = loss * loss_weight
+
+        loss_all = loss, l_fape_side, l_fape_backbone, l_anglenorm, predict_lddt_loss, \
+                   distogram_focal_loss, distogram_regression_loss, plddt2_regression_loss, mask_loss, \
+                   confidence_loss, cameo_loss
+
+        return loss_all
+
+
+class RegressionLosses(nn.Cell):
+    """Return various regressor losses"""
+
+    def __init__(self, first_break, last_break, num_bins, bin_shift=True, beta=0.99, charbonnier_eps=1e-5):
+        super(RegressionLosses, self).__init__()
+
+        self.beta = beta
+        self.charbonnier_eps = charbonnier_eps
+
+        self.first_break = first_break
+        self.last_break = last_break
+        self.num_bins = num_bins
+
+        bin_width = 2
+        start_n = 1
+        stop = self.num_bins * 2
+        centers = mnp.divide(mnp.arange(start=start_n, stop=stop, step=bin_width), self.num_bins * 2.0)
+        self.centers = centers / (self.last_break - self.first_break) + self.first_break
+
+        if bin_shift:
+            centers = mnp.linspace(self.first_break, self.last_break, self.num_bins)
+            self.centers = centers + 0.5 * self.width
+        self.mse = nn.MSELoss()
+        self.mas = nn.L1Loss()
+        self.bmse = BalancedMSE(first_break, last_break, num_bins, beta)
+
+    def construct(self, prediction, target, p_bins=None):
+        """construct"""
+        target = mnp.clip(target, self.centers[0], self.centers[-1])
+
+        mse = self.mse(prediction, target)
+        mae = self.mae(prediction, target)
+        bmse = self.bmse(prediction, target, p_bins=p_bins)
+        return [mse, mae, bmse]
--- MindSPONGE/mindsponge/python/metrics/metrics.py
+++ MindSPONGE/mindsponge/python/metrics/metrics.py
@@ -23,7 +23,15 @@
 """
 Metrics for collective variables
 """
+import numpy as np
+import mindspore.common.dtype as mstype
+import mindspore.communication.management as D
+import mindspore.nn as nn
+import mindspore.numpy as mnp
 
+from mindspore import Parameter, Tensor
+from mindspore.ops import functional as F
+from mindspore.ops import operations as P
 from mindspore.nn import Metric
 
 from ..colvar import Colvar
@@ -51,3 +59,229 @@ class CV(Metric):
 
     def eval(self):
         return self._cv_value
+
+
+class BalancedMSE(nn.Cell):
+    """Balanced MSE error
+    Compute Balanced MSE error between the prediction and the ground truth
+     to solve unbalanced labels in regression task.
+    Ren, Jiawei, et al. "Balanced MSE for Imbalanced Visual Regression."
+
+    Args:
+      prediction: A float tensor of size [batch, ...].
+      target: A float tensor of size [batch, ...].
+
+    Returns:
+      A float tensor representing balanced error
+    """
+
+    def __init__(self, first_break, last_break, num_bins, beta=0.99):
+        super(BalancedMSE, self).__init__()
+        self.beta = beta
+        self.first_break = first_break
+        self.last_break = last_break
+        self.num_bins = num_bins
+
+        self.breaks = mnp.linspace(self.first_break, self.last_break, self.num_bins)
+        self.width = self.breaks[1] - self.breaks[0]
+
+        self.log_noise_scale = Parameter(Tensor([0.], mstype.float32))
+        self.p_bins = Parameter(Tensor(np.ones((self.num_bins)) / self.num_bins, dtype=mstype.float32), \
+                                   name='p_bins', requires_grad=False)
+
+        self.softmax = nn.Softmax(-1)
+        self.zero = Tensor([0.])
+
+        self.onehot = nn.OneHot(depth=self.num_bins)
+        self.allreduce = P.AllReduce()
+        self.device_num = D.get_group_size()
+
+    def construct(self, prediction, target, p_bins=None):
+        """construct"""
+        if p_bins is None:
+            p_bins = self._compute_p_bins(prediction)
+
+        log_sigma2 = self.log_noise_scale * 1.
+        log_sigma2 = 5. * P.Tanh()(log_sigma2 / 5.)
+        sigma2 = mnp.exp(log_sigma2) + 0.25 * self.width
+        tau = 2. * sigma2
+        a = - F.square(prediction - target) / tau
+
+        ndim = prediction.ndim
+        y_bins = mnp.reshape(self.centers * 1., ndim * (1,) + (-1,))
+        b_term = - F.square(mnp.expand_dims(prediction, -1) - y_bins) / tau
+
+        p_clip = mnp.clip(p_bins, 1e-8, 1 - 1e-8)
+        log_p = mnp.log(p_clip)
+        log_p = mnp.reshape(log_p, ndim * (1,) + (-1,))
+
+        b_term += log_p
+        b = nn.ReduceLogSumExp(-1, False)(b_term)
+
+        err = -a + b
+        return err
+
+    def _compute_p_bins(self, y_gt):
+        """compute bins"""
+        ndim = y_gt.ndim
+        breaks = mnp.reshape(self.breaks, (1,) * ndim + (-1,))
+        y_gt = mnp.expand_dims(y_gt, -1)
+
+        y_bins = (y_gt > breaks).astype(mstype.float32)
+        y_bins = P.ReduceSum()(y_bins, -1).astype(mstype.int32)
+        p_gt = self.onehot(y_bins)
+
+        p_gt = P.Reshape()(p_gt, (-1, self.num_bins))
+        p_bins = P.ReduceMean()(p_gt, 0)
+        p_bins = self.allreduce(p_bins) / self.device_num
+
+        p_bins = self.beta * self.p_bins + (1 - self.beta) * p_bins
+        P.Assign()(self.p_bins, p_bins)
+
+        return p_bins
+
+
+class MultiClassFocal(nn.Cell):
+    """Focal error for multi-class classifications.
+
+    Compute the multiple classes focal error between `prediction` and the ground truth `target`.
+
+    Lin, Tsung-Yi, et al. "Focal loss for dense object detection."
+
+    Args:
+      prediction: A float tensor of size [batch, ...].
+      target: A float tensor of size [batch, ...].
+
+    Returns:
+      A scalar representing normalized total error.
+    """
+
+    def __init__(self, num_class=10, beta=0.99, gamma=2., e=0.1, neighbors=2, not_focal=False):
+        super(MultiClassFocal, self).__init__()
+        self.num_class = num_class
+        self.beta = beta
+        self.gamma = gamma
+        self.e = e
+        self.neighbors = neighbors
+        self.not_focal = not_focal
+
+        neighbor_mask = np.ones((self.num_class, self.num_class))
+        neighbor_mask = neighbor_mask - np.triu(neighbor_mask, neighbors) - np.tril(neighbor_mask, -neighbors)
+        neighbor_mask = neighbor_mask / (np.sum(neighbor_mask, axis=-1, keepdims=True) + 1e-10)
+        self.neighbor_mask = Tensor(neighbor_mask, mstype.float32)
+
+        self.class_weights = Parameter(Tensor(np.ones((self.num_class)) / self.num_class, dtype=mstype.float32), \
+                                          name='class_weights', requires_grad=False)
+
+        self.softmax = nn.Softmax(-1)
+        self.cross_entropy = P.SoftmaxCrossEntropyWithLogits()
+        self.zero = Tensor([0.])
+
+        self.allreduce = P.AllReduce()
+        self.device_num = D.get_group_size()
+
+    def construct(self, prediction, target):
+        """construct"""
+        prediction_tensor = self.softmax(prediction)
+
+        zeros = mnp.zeros_like(prediction_tensor)
+        one_minus_p = mnp.where(target > 1e-5, target - prediction_tensor, zeros)
+        ft = -1 * mnp.pow(one_minus_p, self.gamma) * mnp.log(mnp.clip(prediction_tensor, 1e-8, 1.0))
+
+        classes_num = self._compute_classes_num(target)
+        total_num = mnp.sum(classes_num)
+
+        classes_w_t1 = total_num / classes_num
+        sum_ = mnp.sum(classes_w_t1)
+        classes_w_t2 = classes_w_t1 / sum_
+        classes_w_tensor = F.cast(classes_w_t2, mstype.float32)
+
+        weights = self.beta * self.class_weights + (1 - self.beta) * classes_w_tensor
+        P.Assign()(self.class_weights, weights)
+
+        classes_weight = mnp.broadcast_to(mnp.expand_dims(weights, 0), target.shape)
+        alpha = mnp.where(target > zeros, classes_weight, zeros)
+
+        balanced_fl = alpha * ft
+        balanced_fl = mnp.sum(balanced_fl, -1)
+
+        labels = P.MatMul()(target, self.neighbor_mask)
+        xent, _ = self.cross_entropy(prediction, target)
+
+        final_loss = (1 - self.e) * balanced_fl + self.e * xent
+
+        if self.not_focal:
+            softmax_xent, _ = self.cross_entropy(prediction, labels)
+            final_loss = (1 - self.e) * softmax_xent + self.e * xent
+
+        return final_loss
+
+    def _compute_classes_num(self, target):
+        "get global classes number"
+        classes_num = mnp.sum(target, 0)
+        classes_num = self.allreduce(classes_num)
+        classes_num = F.cast(classes_num, mstype.float32)
+        classes_num += 1.
+        return classes_num
+
+
+class BinaryFocal(nn.Cell):
+    """Focal error for Binary classifications.
+    Compute the binary classes focal error between `prediction` and the ground truth `target`.
+
+    Lin, Tsung-Yi, et al. "Focal loss for dense object detection."
+
+    Focal error = -alpha_t * (1-pt)^gamma * log(pt)
+    where pt is the probability of being classified to the true class.
+    pt = p (if true class), otherwise pt = 1 - p.
+
+    Args:
+      prediction: A float tensor of size [batch, ...].
+      target: A float tensor of size [batch, ...].
+      alpha: A float tensor specifying per-example weight for balanced cross entropy.
+      gamma: A float scalar modulating loss from hard and easy examples.
+
+    Returns:
+      A scalar representing normalized total error.
+    """
+
+    def __init__(self, alpha=0.25, gamma=2., feed_in=False, not_focal=False):
+        super(BinaryFocal, self).__init__()
+        self.alpha = alpha
+        self.gamma = gamma
+        self.feed_in = feed_in
+        self.not_focal = not_focal
+
+        self.cross_entropy = P.BinaryCrossEntropy(reduction='none')
+        self.sigmoid = P.Sigmoid()
+        self.epsilon = 1e-8
+
+    def construct(self, prediction, target):
+        """construct"""
+        epsilon = self.epsilon
+        target = F.cast(target, mstype.float32)
+        probs = F.cast(prediction, mstype.float32)
+        if self.feed_in:
+            probs = self.sigmoid(prediction)
+        else:
+            prediction = self._convert(prediction)
+
+        ones_tensor = mnp.ones_like(target)
+        positive_pt = mnp.where(target > 1e-5, probs, ones_tensor)
+        negative_pt = mnp.where(target < 1e-5, 1 - probs, ones_tensor)
+
+        focal_loss = -self.alpha * mnp.pow(1 - positive_pt, self.gamma) * \
+                     mnp.log(mnp.clip(positive_pt, epsilon, 1.)) - (1 - self.alpha) * \
+                     mnp.pow(1 - negative_pt, self.gamma) * mnp.log(mnp.clip(negative_pt, epsilon, 1.))
+        focal_loss *= 2.
+
+        if self.not_focal:
+            focal_loss = self.cross_entropy(prediction, target, ones_tensor)
+
+        return focal_loss
+
+    def _convert(self, probs):
+        """convert function"""
+        probs = mnp.clip(probs, 1e-5, 1. - 1e-5)
+        prediction = mnp.log(probs / (1 - probs))
+        return prediction
