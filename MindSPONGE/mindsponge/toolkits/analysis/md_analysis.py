a new application case (FEP calculation) and fix some bugs

MindSPONGE/mindsponge/toolkits/analysis/md_analysis.py
@@ -3,7 +3,7 @@ This **module** gives functions and classes to use MDAnalysis to analyze the tra
 """
 import os.path
 import numpy as np
-from ..helper import Xopen, set_global_alternative_names
+from ..helper import Xopen, set_global_alternative_names, set_attribute_alternative_name
 try:
     from MDAnalysis.coordinates import base
     from MDAnalysis.lib import util
@@ -12,13 +12,9 @@ except ModuleNotFoundError as exc:
         "'MDAnalysis' package needed. Maybe you need 'pip install MDAnalysis'") from exc
 
 
-class _SpongeTrajectoryReader(base.ReaderBase):
+class SpongeTrajectoryReader(base.ReaderBase):
     """
     This **class** is the interface to MDAnalysis.
-
-    .. TIP::
-
-        You do not need to call the class yourself. Use the function set_universe_sponge_trajectory.
 
     :param dat_file_name: the name of the SPONGE dat trajectory file
     :param box: the name of the box file or a list of 6 ``int`` or ``float`` \
@@ -133,20 +129,7 @@ representing the 3 box lengths and 3 box angles.
         self._offsets.pop()
 
 
-def set_universe_sponge_trajectory(universe, trajname, box, **kwargs):
-    """
-    This **function** sets the trajectory to an MDAnalysis.Universe instance
-
-    :param universe: the MDAnalysis.Universe object
-    :param trajname: the name of the trajectory file
-    :param box: the name of the box file or a list of 6 ``int`` or ``float`` \
-representing the 3 box lengths and 3 box angles.
-    :return: None
-    """
-    setattr(universe, "_trajectory", _SpongeTrajectoryReader(trajname, box, len(u.atoms), **kwargs))
-
-
-class TrajWriter():
+class SpongeTrajectoryWriter():
     """
     This **class** is used to write the SPONGE trajectory (xxx.dat and xxx.box)
 
@@ -158,7 +141,7 @@ class TrajWriter():
 
         u = mda.Universe(PDB, XTC)
 
-        with xmda.TrajWriter("mda_test") as W:
+        with xmda.SpongeTrajectoryWriter("mda_test") as W:
             for ts in u.trajectory:
                 W.write(u)
 
@@ -207,5 +190,158 @@ class TrajWriter():
         """
         self.datfile.write(u.coord.positions.astype(np.float32).tobytes())
         self.boxfile.write(" ".join([f"{i}" for i in u.coord.dimensions]) + "\n")
+
+
+class SpongeCoordinateReader(base.ReaderBase):
+    """
+    This **class** is the interface to MDAnalysis.
+
+    :param file_name: the name of the SPONGE coordinate trajectory file
+    """
+    def __init__(self, file_name, n_atoms, **kwargs):
+        super().__init__(file_name, **kwargs)
+        self._n_atoms = n_atoms
+        self._n_frames = 1
+        self.file = None
+        self.start = 0
+        self.ts = base.Timestep(self.n_atoms, **self._ts_kwargs)
+        self._read_next_timestep()
+
+    @property
+    def n_frames(self):
+        """
+        The total number of frames in the trajectory file
+        """
+        return self._n_frames
+
+    @property
+    def n_atoms(self):
+        """
+        The total number of atoms in the trajectory file
+        """
+        return self._n_atoms
+
+    def close(self):
+        """
+        Close all the opened file
+
+        :return: None
+        """
+        if self.file is not None:
+            self.file.close()
+            self.file = None
+
+    def open_file(self):
+        """
+        Open the coordinate file
+
+        :return: trajectory file and box file
+        """
+        self.file = util.anyopen(self.filename, "r")
+        self.file.readline()
+        self.start = self.file.tell()
+
+    def _reopen(self):
+        self.close()
+        self.open_file()
+
+    def _read_frame(self, frame):
+        """
+
+        :param frame:
+        :return:
+        """
+        if self.file is None:
+            self.open_file()
+        self.file.seek(self.start)
+        self.ts.frame = frame - 1
+        return self._read_next_timestep()
+
+    def _read_next_timestep(self):
+        """
+
+        :return:
+        """
+        ts = self.ts
+        if self.file is None:
+            self.open_file()
+        if self.file.tell() != self.start:
+            raise EOFError
+        t = np.loadtxt(self.file, max_rows=self.n_atoms)
+        setattr(ts, "_pos", t)
+        box = list(map(float, self.file.readline().split()))
+        ts.dimensions = box
+        ts.frame += 1
+        return ts
+
+
+class SpongeCoordinateWriter():
+    """
+    This **class** is used to write the SPONGE coordinate file
+
+    usage example::
+
+        import Xponge.analysis.md_analysis as xmda
+        import MDAnalysis as mda
+        from MDAnalysis.tests.datafiles import PDB, XTC
+
+        u = mda.Universe(PDB, XTC)
+
+        with xmda.SpongeCoordinateWriter("mda_test") as W:
+            for ts in u.trajectory:
+                W.write(u)
+
+    :param file_name: the name of the output file
+    :param n_atoms: the total number of atoms this Timestep describes
+    """
+    def __init__(self, file_name, n_atoms=None):
+        self.filename = file_name
+        self.file = None
+        self.n_atoms = n_atoms
+        set_attribute_alternative_name(self, self.open)
+        set_attribute_alternative_name(self, self.close)
+        set_attribute_alternative_name(self, self.write)
+
+    def __enter__(self):
+        self.open()
+        return self
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        self.close()
+
+    def open(self):
+        """
+        This **function** opens the coordinate file
+
+        :return: None
+        """
+        self.file = Xopen(self.filename, "w")
+
+    def close(self):
+        """
+        This **function** closes the coordinate file
+
+        :return: None
+        """
+        self.file.close()
+
+    def write(self, u):
+        """
+        This **function** writes the coordinates of the Universe to the output files
+
+        :param u: an MDAnalysis.Universe instance
+        :return: None
+        """
+        if self.n_atoms is None:
+            self.n_atoms = len(u.coord.positions)
+        towrite = f"{self.n_atoms}\n"
+        for crd in u.coord.positions[:self.n_atoms]:
+            towrite += f"{crd[0]} {crd[1]} {crd[2]}\n"
+        if u.coord.dimensions:
+            towrite += " ".join([f"{i}" for i in u.coord.dimensions]) + "\n"
+        else:
+            towrite += "999 999 999 90 90 90\n"
+        self.file.write(towrite)
+
 
 set_global_alternative_names()

MindSPONGE/mindsponge/toolkits/process.py
@@ -230,7 +230,7 @@ def h_mass_repartition(molecules, repartition_mass=1.1, repartition_rate=3, excl
                     res.name2atom(heavy_atom.name).mass -= delta_mass
 
 
-def solvent_replace(molecule, select, toreplace):
+def solvent_replace(molecule, select, toreplace, sort=True):
     """
     This **function** replaces the solvent to some other molecules randomly.
 
@@ -241,18 +241,31 @@ def solvent_replace(molecule, select, toreplace):
         import Xponge.forcefield.amber.tip3p
         mol = ALA*10
         Add_Solvent_Box(mol, WAT, 10)
-        Solvent_Replace(mol, lambda res:res.name == "WAT", {K:10, CL:10})
+        Solvent_Replace(mol, WAT, {K:10, CL:10})
+        #Solvent_Replace(mol, lambda res:res.name == "WAT", {K:10, CL:10})
 
     :param molecule: a ``Molecule`` instance
-    :param select: a **function** to decide which residues should be replaced
+    :param select: a **function** to decide which residues should be replaced, \
+or a Residue, a ResidueType or a Molecule with only one Residue, \
+which the residues to be replaced have the same name
     :param toreplace: a dict, which stores the mapping of molecules to replace and the number of molecules. \
  Every molecule should be a ``ResidueType``, a ``Residue`` or a ``Molecule`` with only one ``Residue``.
+    :param sort: whether to sort the residues after replacing
     :return: None
     """
     solvents = []
+    for_sort = Xdict()
+    if not callable(select):
+        if isinstance(select, Molecule):
+            select = select.residues[0]
+        resname = select.name
+        select = lambda res: res.name == resname
     for i, resi in enumerate(molecule.residues):
         if select(resi):
             solvents.append(i)
+            for_sort[resi] = float("inf")
+        else:
+            for_sort[resi] = float("-inf")
 
     np.random.shuffle(solvents)
     count = 0
@@ -271,6 +284,9 @@ def solvent_replace(molecule, select, toreplace):
                 new_residue.Add_Atom(atom, x=atom.x + crd_o[0] - crd0[0],
                                      y=atom.y + crd_o[1] - crd0[1], z=atom.z + crd_o[2] - crd0[2])
             molecule.residues[i] = new_residue
+            for_sort[new_residue] = count
+    if sort:
+        molecule.residues.sort(key=lambda res: for_sort[res])
 
 
 def main_axis_rotate(molecule, direction_long=None, direction_middle=None, direction_short=None):

MindSPONGE/mindsponge/toolkits/tools/__init__.py
@@ -1,7 +1,8 @@
 """
 This **module** implements the terminal commands
 """
-from ..helper import source, GlobalSetting, Xopen
+from ..helper import source, GlobalSetting, Xopen, Xprint
+from ..mdrun import run
 
 
 def _basic_test(args):
@@ -94,6 +95,39 @@ def test(args):
 
     if "charmm27" in args.do:
         _charmm27_test(args)
+
+
+def converter(args):
+    """
+    This **function** converts the format of coordinate file
+
+    :param args: arguments from argparse
+    :return: None
+    """
+    from ..analysis import md_analysis as xmda
+    import MDAnalysis as mda
+
+    if args.c:
+        if args.cf == "guess":
+            u = mda.Universe(args.p, args.c)
+        elif args.cf == "sponge_crd":
+            u = mda.Universe(args.p, args.c, format=xmda.SpongeCoordinateReader)
+        elif args.cf == "sponge_traj":
+            u = mda.Universe(args.p, args.c, format=xmda.SpongeTrajectoryReader)
+    else:
+        u = mda.Universe(args.p)
+
+    if args.of == "sponge_crd":
+        with xmda.SpongeCoordinateWriter(args.o) as w:
+            w.write(u)
+    elif args.of == "sponge_traj":
+        with xmda.SpongeTrajectoryWriter(args.o) as w:
+            for _ in u.trajectory:
+                w.write(u)
+    else:
+        with mda.Writer(args.o, n_atoms=len(u.coord.positions)) as w:
+            for _ in u.trajectory:
+                w.write(u)
 
 
 def maskgen(args):
@@ -250,8 +284,8 @@ def name2name(args):
         from_ = assign.Get_Assignment_From_PDB(args.from_file, determine_bond_order=False,
                                                only_residue=args.from_residue)
 
-    rdmol_a = rdktool.Assign2RDKitMol(to_, True)
-    rdmol_b = rdktool.Assign2RDKitMol(from_, True)
+    rdmol_a = rdktool.assign_to_rdmol(to_, True)
+    rdmol_b = rdktool.assign_to_rdmol(from_, True)
 
     result = rdFMCS.FindMCS([rdmol_a, rdmol_b], completeRingsOnly=True, timeout=args.tmcs)
     rdmol_mcs = Chem.MolFromSmarts(result.smartsString)
@@ -294,7 +328,7 @@ def _mol2rfe_build(args, merged_from, merged_to):
     min_ = source("..forcefield.special.min")
 
     if "build" in args.do:
-        print("\nBUILDING TOPOLOGY\n")
+        Xprint("\nBUILDING TOPOLOGY\n", verbose=-1)
         fep.Save_Soft_Core_LJ()
 
         for i in range(args.nl + 1):
@@ -306,7 +340,7 @@ def _mol2rfe_build(args, merged_from, merged_to):
                 min_.save_min_bonded_parameters()
             elif i == 1:
                 min_.do_not_save_min_bonded_parameters()
-            Xponge.BUILD.Save_SPONGE_Input(tt, "%d/%s" % (i, args.temp))
+            build.Save_SPONGE_Input(tt, "%d/%s" % (i, args.temp))
 
 
 def _mol2rfe_output_path(subdir, workdir, tempname):
@@ -335,33 +369,29 @@ def _mol2rfe_min(args):
             if os.path.exists("%d/min" % i):
                 os.system("rm -rf %d/min" % i)
             os.mkdir("%d/min" % i)
-            basic = f"{args.sponge} -default_in_file_prefix {i}/{args.temp}"
+            basic = f"SPONGE -default_in_file_prefix {i}/{args.temp}"
             lambda_ = i / args.nl
             basic += f" -mode minimization -lambda_lj {lambda_}"
             basic += _mol2rfe_output_path("min", i, args.temp)
-            if i != 0 and args.mlast:
-                cif = " -coordinate_in_file {1}/min/{0}_coordinate.txt".format(args.temp, i - 1)
-                cif += " -constrain_mode SHAKE"
-            else:
-                cif = f" -mass_in_file 0/{args.temp}_fake_mass.txt"
-                os.system(f"{basic} {cif} -minimization_dynamic_dt 1 -step_limit {args.m1steps[0]}")
+            if not args.mi:
+                cif = " -constrain_mode SHAKE"
+                run(f"{basic} {cif} -dt 1e-8 -step_limit {args.msteps[0]}")
                 cif += " -coordinate_in_file {1}/min/{0}_coordinate.txt".format(args.temp, i)
-                os.system(f"{basic} {cif} -dt 1e-7 -step_limit {args.m1steps[1]}")
-                os.system(f"{basic} {cif} -dt 1e-6 -step_limit {args.m1steps[2]}")
-                os.system(f"{basic} {cif} -dt 1e-5 -step_limit {args.m1steps[3]}")
-                os.system(f"{basic} {cif} -dt 1e-4 -step_limit {args.m1steps[4]}")
-                cif = " -coordinate_in_file {1}/min/{0}_coordinate.txt -constrain_mode SHAKE".format(args.temp, i)
-                os.system(f"{basic} {cif} -minimization_dynamic_dt 1 -step_limit {args.m2steps[0]}")
-                os.system(f"{basic} {cif} -dt 1e-7 -step_limit {args.m2steps[1]}")
-                os.system(f"{basic} {cif} -dt 1e-6 -step_limit {args.m2steps[2]}")
-                os.system(f"{basic} {cif} -dt 1e-5 -step_limit {args.m2steps[3]}")
-                os.system(f"{basic} {cif} -dt 1e-4 -step_limit {args.m2steps[4]}")
+                run(f"{basic} {cif} -dt 1e-7 -step_limit {args.msteps[1]}")
+                run(f"{basic} {cif} -dt 1e-6 -step_limit {args.msteps[2]}")
+                run(f"{basic} {cif} -dt 1e-5 -step_limit {args.msteps[3]}")
+                run(f"{basic} {cif} -dt 1e-4 -step_limit {args.msteps[4]}")
+                run(f"{basic} {cif} -dt 1e-3 -step_limit {args.msteps[5]}")
+            else:
+                mdin = args.mi.pop(0)
+                cif = ""
+                run(f"{basic} {cif} -mdin {mdin}")
+                cif += " -coordinate_in_file {1}/min/{0}_coordinate.txt".format(args.temp, i)
+                for mdin in args.mi:
+                    run(f"{basic} {cif} -mdin {mdin}")
 
-            os.system(f"{basic} {cif} -minimization_dynamic_dt 1 -step_limit {args.msteps[0]}")
-            os.system(f"{basic} {cif} -minimization_dynamic_dt 1e-3 -step_limit {args.msteps[1]}")
 
-
-def _mol2rfe_prebalance(args):
+def _mol2rfe_pre_equilibrium(args):
     """
 
     :param args:
@@ -369,26 +399,26 @@ def _mol2rfe_prebalance(args):
     """
     source("..")
 
-    if "prebalance" in args.do:
+    if "pre_equilibrium" in args.do:
         for i in range(args.nl + 1):
-            if os.path.exists("%d/prebalance" % i):
-                os.system("rm -rf %d/prebalance" % i)
-            os.mkdir("%d/prebalance" % i)
-            command = f"{args.sponge} -default_in_file_prefix {i}/{args.temp}"
+            if os.path.exists("%d/pre_equilibrium" % i):
+                os.system("rm -rf %d/pre_equilibrium" % i)
+            os.mkdir("%d/pre_equilibrium" % i)
+            command = f"SPONGE -default_in_file_prefix {i}/{args.temp}"
             lambda_ = i / args.nl
             command += f" -lambda_lj {lambda_}"
-            command += _mol2rfe_output_path("prebalance", i, args.temp)
+            command += _mol2rfe_output_path("pre_equilibrium", i, args.temp)
             command += f" -coordinate_in_file {i}/min/{args.temp}_coordinate.txt"
             if not args.pi:
-                command += f" -mode NPT -step_limit {args.prebalance_step} -dt {args.dt} -constrain_mode SHAKE"
+                command += f" -mode NPT -step_limit {args.pre_equilibrium_step} -dt {args.dt} -constrain_mode SHAKE"
                 command += f" -barostat {args.barostat} -thermostat {args.thermostat}"
-                os.system(command)
+                run(command)
             else:
                 command += f" -mdin {args.pi}"
-                os.system(command)
+                run(command)
 
 
-def _mol2rfe_balance(args):
+def _mol2rfe_equilibrium(args):
     """
 
     :param args:
@@ -396,23 +426,23 @@ def _mol2rfe_balance(args):
     """
     source("..")
 
-    if "balance" in args.do:
+    if "equilibrium" in args.do:
         for i in range(args.nl + 1):
-            if os.path.exists("%d/balance" % i):
-                os.system("rm -rf %d/balance" % i)
-            os.mkdir("%d/balance" % i)
-            command = f"{args.sponge} -default_in_file_prefix {i}/{args.temp}"
+            if os.path.exists("%d/equilibrium" % i):
+                os.system("rm -rf %d/equilibrium" % i)
+            os.mkdir("%d/equilibrium" % i)
+            command = f"SPONGE -default_in_file_prefix {i}/{args.temp}"
             lambda_ = i / args.nl
             command += f" -lambda_lj {lambda_}"
-            command += _mol2rfe_output_path("balance", i, args.temp)
-            command += f" -coordinate_in_file {i}/prebalance/{args.temp}_coordinate.txt"
+            command += _mol2rfe_output_path("equilibrium", i, args.temp)
+            command += f" -coordinate_in_file {i}/pre_equilibrium/{args.temp}_coordinate.txt"
             if not args.bi:
-                command += f" -mode NPT -step_limit {args.balance_step} -dt {args.dt} -constrain_mode SHAKE"
+                command += f" -mode NPT -step_limit {args.equilibrium_step} -dt {args.dt} -constrain_mode SHAKE"
                 command += f" -barostat {args.barostat} -thermostat {args.thermostat}"
-                os.system(command)
+                run(command)
             else:
                 command += f" -mdin {args.pi}"
-                os.system(command)
+                run(command)
 
 
 def _mol2rfe_analysis(args, merged_from):
@@ -434,7 +464,7 @@ def _mol2rfe_analysis(args, merged_from):
                     os.system("rm -rf %d/ti" % i)
                 os.mkdir("%d/ti" % i)
                 inprefix = f"{i}/{args.temp}"
-                command = f"{args.sponge_ti} -LJ_soft_core_in_file {inprefix}_LJ_soft_core.txt"
+                command = f"SPONGE_TI -LJ_soft_core_in_file {inprefix}_LJ_soft_core.txt"
                 command += " -exclude_in_file {0}_exclude.txt -charge_in_file {0}_charge.txt".format(inprefix)
                 command += f" -chargeA_in_file 0/{args.temp}_charge.txt"
                 command += f" -chargeB_in_file {args.nl}/{args.temp}_charge.txt"
@@ -443,15 +473,15 @@ def _mol2rfe_analysis(args, merged_from):
                 command += f" -subsys_division_in_file {inprefix}_subsys_division.txt  -charge_pertubated 1"
                 inprefix = f"{i}/ti/{args.temp}"
                 command += f" -mdinfo {inprefix}.mdinfo -mdout {inprefix}.mdout"
-                inprefix = f"{i}/balance/{args.temp}"
+                inprefix = f"{i}/equilibrium/{args.temp}"
                 command += f" -crd {inprefix}.dat -box {inprefix}.box -TI dh_dlambda.txt"
                 command += f" -atom_numbers {len(merged_from.atoms)}"
-                command += f" -frame_numbers {args.balance_step // 100}"
+                command += f" -frame_numbers {args.equilibrium // 100}"
                 if not args.ai:
-                    os.system(command)
+                    run(command)
                 else:
                     command += f" -mdin {args.ai}"
-                    os.system(command)
+                    run(command)
             dh_dlambda = np.loadtxt("dh_dlambda.txt")
             dh = []
             dh_int = []
@@ -493,7 +523,7 @@ def mol2rfe(args):
         __import__(ipy)
 
     if not args.do:
-        args.do = [["build", "min", "prebalance", "balance", "analysis"]]
+        args.do = [["build", "min", "pre_equilibrium", "equilibrium", "analysis"]]
     args.do = args.do[0]
 
     from_res_type_ = load_mol2(args.r1).residues[0]
@@ -501,8 +531,8 @@ def mol2rfe(args):
     if not args.ff:
         parmchk2_gaff(args.r1, args.temp + "_TMP1.frcmod")
 
-    to_res_type_ = Xponge.load_mol2(args.r2).residues[0]
-    to_ = Xponge.assign.Get_Assignment_From_ResidueType(to_res_type_)
+    to_res_type_ = load_mol2(args.r2).residues[0]
+    to_ = assign.Get_Assignment_From_ResidueType(to_res_type_)
     if not args.ff:
         parmchk2_gaff(args.r2, args.temp + "_TMP2.frcmod")
 
@@ -517,12 +547,12 @@ def mol2rfe(args):
         H_Mass_Repartition(merged_from)
         H_Mass_Repartition(merged_to)
 
-    mol2rfe_build(args, merged_from, merged_to)
+    _mol2rfe_build(args, merged_from, merged_to)
 
-    mol2rfe_min(args)
+    _mol2rfe_min(args)
 
-    mol2rfe_prebalance(args)
+    _mol2rfe_pre_equilibrium(args)
 
-    mol2rfe_balance(args)
+    _mol2rfe_equilibrium(args)
 
-    mol2rfe_analysis(args, merged_from)
+    _mol2rfe_analysis(args, merged_from)