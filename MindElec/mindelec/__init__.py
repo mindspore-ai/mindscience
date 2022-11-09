cd36f580caebed85eb345783a7b1a1a3cba7065b
melody <chenmengyun1@huawei.com>
2022-11-09 17:10:28 +0800

    Add MindSpore version check when run the MindSpore Elec

--- MindElec/mindelec/__init__.py
+++ MindElec/mindelec/__init__.py
@@ -21,3 +21,42 @@ from .common import *
 __all__ = []
 __all__.extend(solver.__all__)
 __all__.extend(common.__all__)
+
+
+def _mindspore_version_check():
+    """
+    Do the MindSpore version check for MindSpore Elec. If the
+    MindSpore can not be imported, it will raise ImportError. If its
+    version is not compatibale with current MindSpore Elec verision,
+    it will print a warning.
+
+    Raise:
+        ImportError: If the MindSpore can not be imported.
+    """
+
+    try:
+        import mindspore as ms
+        from mindspore import log as logger
+    except (ImportError, ModuleNotFoundError):
+        print("Can not find MindSpore in current environment. Please install "
+              "MindSpore before using MindSpore Elec, by following "
+              "the instruction at https://www.mindspore.cn/install")
+        raise
+
+    ms_version = ms.__version__
+    ms_requires = '1.5.0'
+
+    if ms_version < ms_requires:
+        logger.warning("Current version of MindSpore is not compatible with MindSpore Elec. "
+                       "Some functions might not work or even raise error. Please install MindSpore "
+                       "version >= 1.5.0. For more details about dependency setting, please check "
+                       "the instructions at MindSpore official website https://www.mindspore.cn/install "
+                       "or check the README.md at https://gitee.com/mindspore/mindscience/tree/master/MindElec")
+        warning_countdown = 3
+        for i in range(warning_countdown, 0, -1):
+            logger.warning(
+                f"Please pay attention to the above warning, countdown: {i}")
+            time.sleep(1)
+
+
+_mindspore_version_check()
