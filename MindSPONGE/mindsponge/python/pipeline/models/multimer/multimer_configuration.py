57dd6e61a36e4f3b452163c3ca29fce5fa346696
l00500167 <lijunbin4@huawei.com>
2023-02-25 11:17:35 +0800

    add multimer yaml Signed-off-by: l00500167 <lijunbin4@huawei.com>

--- MindSPONGE/mindsponge/python/pipeline/models/multimer/multimer_configuration.py
+++ MindSPONGE/mindsponge/python/pipeline/models/multimer/multimer_configuration.py
@@ -22,5 +22,11 @@
 # ============================================================================
 """multimer configuration"""
 multimer_configuration = {
-    "predict_256": "https://download.mindspore.cn/mindscience/mindsponge/Multimer/config/"
+    "predict_256": "https://download.mindspore.cn/mindscience/mindsponge/Multimer/config/predict_256.yaml",
+    "predict_512": "https://download.mindspore.cn/mindscience/mindsponge/Multimer/config/predict_512.yaml",
+    "predict_768": "https://download.mindspore.cn/mindscience/mindsponge/Multimer/config/predict_768.yaml",
+    "predict_1024": "https://download.mindspore.cn/mindscience/mindsponge/Multimer/config/predict_1024.yaml",
+    "predict_1280": "https://download.mindspore.cn/mindscience/mindsponge/Multimer/config/predict_1280.yaml",
+    "predict_1536": "https://download.mindspore.cn/mindscience/mindsponge/Multimer/config/predict_1536.yaml",
+    "predict_1792": "https://download.mindspore.cn/mindscience/mindsponge/Multimer/config/predict_1792.yaml"
 }
