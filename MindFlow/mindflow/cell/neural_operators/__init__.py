e8090d9ed38521f189a7d5e2c635bc8302d6d7f4
chengzrz <czrzrichard@gmail.com>
2023-04-06 05:40:18 +0800

    add fno3d model
    Signed-off-by: chengzrz <czrzrichard@gmail.com>

--- MindFlow/mindflow/cell/neural_operators/__init__.py
+++ MindFlow/mindflow/cell/neural_operators/__init__.py
@@ -15,10 +15,11 @@
 """init"""
 from .fno1d import FNO1D
 from .fno2d import FNO2D
+from .fno3d import FNO3D
 from .kno1d import KNO1D
 from .kno2d import KNO2D
 from .pdenet import PDENet
 
-__all__ = ["FNO1D", "FNO2D", "KNO1D", "KNO2D", "PDENet"]
+__all__ = ["FNO1D", "FNO2D", "FNO3D", "KNO1D", "KNO2D", "PDENet"]
 
 __all__.sort()
