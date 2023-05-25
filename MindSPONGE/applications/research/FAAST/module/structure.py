8f3e49670227397cc65591bd26948720f6af6540
cheng-hao-wang <wangchenghao14@huawei.com>
2023-05-25 19:03:43 +0800

    add slice_128

--- MindSPONGE/applications/research/FAAST/module/structure.py
+++ MindSPONGE/applications/research/FAAST/module/structure.py
@@ -420,11 +420,7 @@ class StructureModule(nn.Cell):
             np.arange(self.seq_length).reshape((-1, 1, 1)).repeat(37, axis=1).astype("int32"))
         self.traj_w = Tensor(np.array([1.] * 4 + [self.config.position_scale] * 3), mstype.float32)
         self.use_sumcons = True
-        if self.use_sumcons:
-            print("use_sumcons")
         self.use_recyclecons = False
-        if self.use_recyclecons:
-            print("use_recyclecons")
 
     def construct(self, single, pair, seq_mask, aatype, contact_act2, contact_info_mask2, residx_atom37_to_atom14=None,
                   atom37_atom_exists=None):
