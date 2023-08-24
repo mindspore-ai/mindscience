4588fd341958b08834586346185ce0983c037689
guoboqiang@huawei.com <guoboqiang@huawei.com>
2023-08-24 11:49:11 +0800

    [mod]: pde-net percnn

--- MindFlow/applications/data_mechanism_fusion/PeRCNN/train.py
+++ MindFlow/applications/data_mechanism_fusion/PeRCNN/train.py
@@ -17,23 +17,21 @@
 import argparse
 import time
 import numpy as np
-import imageio
-import matplotlib.pyplot as plt
 
 from mindspore import ops, context, nn, set_seed, save_checkpoint, jit
 import mindspore.common.dtype as mstype
-from mindflow.utils import load_yaml_config
+from mindflow.utils import load_yaml_config, print_log, log_config
 
 from src import RecurrentCNNCell, UpScaler, RecurrentCNNCellBurgers
 from src import Trainer
-from src import post_process_v2
+from src import post_process
 
 set_seed(123456)
 np.random.seed(123456)
 
 
-def train(trainer, stage, config):
-    """train"""
+def train_stage(trainer, stage, pattern, config):
+    """train stage"""
     if 'milestone_num' in config.keys():
         milestone = list([(config['epochs']//config['milestone_num'])*(i + 1)
                           for i in range(config['milestone_num'])])
@@ -75,24 +73,25 @@ def train(trainer, stage, config):
         recurrent_cnn.set_train(True)
         if stage == 'pretrain':
             step_train_loss = train_step()
-            print(
+            print_log(
                 f"epoch: {epoch} train loss: {step_train_loss} epoch time: {(time.time() - time_beg) :.3f} s")
         else:
             step_train_loss, loss_data, loss_ic, loss_phy, loss_valid = train_step()
-            print(f"epoch: {epoch} train loss: {step_train_loss} ic_loss: {loss_ic} data_loss: {loss_data}\
+            print_log(f"epoch: {epoch} train loss: {step_train_loss} ic_loss: {loss_ic} data_loss: {loss_data}\
                    val_loss: {loss_valid} phy_loss: {loss_phy} epoch time: {(time.time() - time_beg): .3f} s")
             if step_train_loss < best_loss:
                 best_loss = step_train_loss
-                print('best loss', best_loss, 'save model')
-                save_checkpoint(upconv, "./model/" + args.pattern +
-                                '/' + config['name_conf'] + "_upconv.ckpt")
-                save_checkpoint(recurrent_cnn, "./model/" + args.pattern +
-                                '/' + config['name_conf'] + "_recurrent_cnn.ckpt")
-    if args.pattern == 'physics_driven':
+                print_log('best loss', best_loss, 'save model')
+                save_checkpoint(upconv, os.path.join("./model", pattern,
+                                                     f"{config['name_conf']}_upconv.ckpt"))
+                save_checkpoint(recurrent_cnn, os.path.join("./model", pattern,
+                                                            f"{config['name_conf']}_recurrent_cnn.ckpt"))
+    if pattern == 'physics_driven':
         trainer.recurrent_cnn.show_coef()
 
 
-if __name__ == '__main__':
+def parse_args():
+    """parse input args"""
     parser = argparse.ArgumentParser(description="burgers train")
     parser.add_argument("--mode", type=str, default="GRAPH", choices=["GRAPH", "PYNATIVE"],
                         help="Running in GRAPH_MODE OR PYNATIVE_MODE")
@@ -106,19 +105,14 @@ if __name__ == '__main__':
     parser.add_argument("--config_file_path", type=str,
                         default="./percnn_burgers.yaml")
     parser.add_argument("--pattern", type=str, default="data_driven")
-    args = parser.parse_args()
+    input_args = parser.parse_args()
+    return input_args
 
-    context.set_context(mode=context.GRAPH_MODE if args.mode.upper().startswith("GRAPH") else context.PYNATIVE_MODE,
-                        save_graphs=args.save_graphs,
-                        save_graphs_path=args.save_graphs_path,
-                        device_target=args.device_target,
-                        device_id=args.device_id,
-                        max_call_depth=99999999)
-    print(
-        f"Running in {args.mode.upper()} mode, using device id: {args.device_id}.")
-    use_ascend = context.get_context(attr_key='device_target') == "Ascend"
 
-    burgers_config = load_yaml_config(args.config_file_path)[args.pattern]
+def train(input_args):
+    """train"""
+    pattern = input_args.pattern
+    burgers_config = load_yaml_config(input_args.config_file_path)[pattern]
 
     upconv = UpScaler(in_channels=burgers_config['input_channel'],
                       out_channels=burgers_config['out_channels'],
@@ -127,7 +121,7 @@ if __name__ == '__main__':
                       stride=burgers_config['stride'],
                       has_bais=True)
 
-    if args.pattern == 'data_driven':
+    if pattern == 'data_driven':
         recurrent_cnn = RecurrentCNNCell(input_channels=burgers_config['input_channel'],
                                          hidden_channels=burgers_config['rcnn_hidden_channel'],
                                          kernel_size=burgers_config['kernel_size'],
@@ -145,37 +139,21 @@ if __name__ == '__main__':
                              nu=burgers_config['nu'],
                              compute_dtype=mstype.float32)
 
-    train(percnn_trainer, 'pretrain', burgers_config['pretrain'])
-    train(percnn_trainer, 'finetune', burgers_config['finetune'])
+    train_stage(percnn_trainer, 'pretrain', pattern, burgers_config['pretrain'])
+    train_stage(percnn_trainer, 'finetune', pattern, burgers_config['finetune'])
+    post_process(percnn_trainer)
 
-    output = percnn_trainer.get_output(1800)
-    output = ops.concat((output, output[:, :, :, 0:1]), axis=3)
-    output = ops.concat((output, output[:, :, 0:1, :]), axis=2)
-    truth_clean = np.concatenate(
-        (percnn_trainer.truth_clean, percnn_trainer.truth_clean[:, :, :, 0:1]), axis=3)
-    truth_clean = np.concatenate(
-        (truth_clean, truth_clean[:, :, 0:1, :]), axis=2)
-    low_res = truth_clean[:, :, ::2, ::2]
-    output = output.asnumpy()
 
-    print(output.shape, truth_clean.shape, low_res.shape)
-
-    err_list = []
-    img_path = []
-    for i in range(0, 1801, 10):
-        err, fig_path = post_process_v2(output, truth_clean, low_res, xmin=0, xmax=1, ymin=0, ymax=1,
-                                        num=i, fig_save_path='figures_' + args.pattern)
-        print('infer step:', i, ', relative l2 error', err)
-        err_list.append([i, err])
-        img_path.append(fig_path)
-    gif_images = []
-    for path in img_path:
-        gif_images.append(imageio.imread(path))
-    imageio.mimsave('results.gif', gif_images, duration=0.01)
-    err_list = np.array(err_list)
-
-    plt.figure(figsize=(6, 4))
-    plt.plot(err_list[:, 0], err_list[:, 1])
-    plt.xlabel('infer_step')
-    plt.ylabel('relative_l2_error')
-    plt.savefig("error.png")
+if __name__ == '__main__':
+    log_config('./logs', 'percnn')
+    args = parse_args()
+    context.set_context(mode=context.GRAPH_MODE if args.mode.upper().startswith("GRAPH") else context.PYNATIVE_MODE,
+                        save_graphs=args.save_graphs,
+                        save_graphs_path=args.save_graphs_path,
+                        device_target=args.device_target,
+                        device_id=args.device_id,
+                        max_call_depth=99999999)
+    print_log("pid:", os.getpid())
+    print_log(f"Running in {args.mode.upper()} mode, using device id: {args.device_id}.")
+    use_ascend = context.get_context(attr_key='device_target') == "Ascend"
+    train(args)
