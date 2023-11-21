9f2ac8d79183219755633f23088355ef21f93b82
brian <guoboqiang@huawei.com>
2023-11-21 10:24:35 +0800

    [add]: navier_stokes st case

--- tests/st/mindflow/networks/burgers/test_burgers.py
+++ tests/st/mindflow/networks/burgers/test_burgers.py
@@ -12,7 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 # ============================================================================
-"""train process"""
+"""burgers pinns st case"""
 
 import pytest
 
