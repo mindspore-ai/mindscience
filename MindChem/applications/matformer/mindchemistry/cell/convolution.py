7fe0fcf6ee02ef85d7a9672e330928c56fbf3774
xukunming2025 <1652262991@qq.com>
2025-10-25 20:58:15 +0800

    规范代码

--- MindChem/applications/matformer/mindchemistry/cell/convolution.py
+++ MindChem/applications/matformer/mindchemistry/cell/convolution.py
@@ -17,8 +17,6 @@ from mindspore import nn, ops, float32
 from mindscience.e3nn.o3 import TensorProduct, Irreps, Linear
 from mindscience.e3nn.nn import FullyConnectedNet
 from ..graph.graph import AggregateEdgeToNode
-from mindscience.e3nn.o3 import TensorProduct, Irreps, Linear
-from mindscience.e3nn.nn import FullyConnectedNet
 
 softplus = ops.Softplus()
 
